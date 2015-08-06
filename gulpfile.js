2190062674e86f8905e521d1e3d8880f5c66f586
ViK <ViK@iMac-Sergey.local>
2015-08-06 09:37:35 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var gulp = require('gulp'),
+    scss = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    webserver = require('gulp-webserver'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    uglify = require('gulp-uglify'),
+    compass = require('gulp-compass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+
+gulp.task('compass', function() {
+  gulp.src('scss/*.scss')
+    .pipe(compass({
+      config_file: 'config.rb',
+      css: 'css',
+      sass: 'scss'
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('autpr', function () {
+    return gulp.src('css/main.css')
+        .pipe(autoprefixer(['> 5%', 'last 5 versions', 'IE 9']))
+        .pipe(gulp.dest('css'));
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('scss/*.scss', ['compass']);
+    gulp.watch('css/*.css', ['autpr']);
+
+});
+
+gulp.task('webserver', function () {
+    gulp.src('')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+
+
+gulp.task('default', [
+    'watch',
+    'compass',
+    'autpr',
+    'webserver'
+]);
+
